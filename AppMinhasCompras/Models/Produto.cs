08f911ca365311616529f29ae32aed5f187c30ba
JuRS-maker <js798853@gmail.com>
2026-08-14 09:25:14 -0300

    Renomeação do model produto e construção da classe crud no sqlite

--- AppMinhasCompras/Models/Produto.cs
+++ AppMinhasCompras/Models/Produto.cs
@@ -5,7 +5,7 @@ namespace AppMinhasCompras.Models
     public class Produto
     {
         [PrimaryKey, AutoIncrement]
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Desricao { get; set; }
         public double Quantidade { get; set; }
         public double Preco {  get; set; }
