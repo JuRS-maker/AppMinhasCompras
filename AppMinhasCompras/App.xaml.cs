7fae24b847174104a681d26d1f55d7cca9564494
JuRS-maker <js798853@gmail.com>
2026-08-21 11:19:30 -0300

    Insert dos produtos, alterações no NovoProduto e ListaProduto.

--- AppMinhasCompras/App.xaml.cs
+++ AppMinhasCompras/App.xaml.cs
@@ -1,9 +1,30 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using AppMinhasCompras.Helpers;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get 
+            { 
+                if (_db == null) 
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
